034a9f078eff56f2dbf853eea7acadba2f6d7f61
hasankarim18 <hallysust@gmail.com>
2023-04-08 10:23:31 +0600

    new name

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+      <div>
+      
+          <h1>Unexpected Application Error!</h1>
+          <h2>404 Not Found</h2>
+       
+      </div>
+    );
+};
+
+export default ErrorPage;
